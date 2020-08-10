97e1cbc5c5a165c6e3b299e9bdeb12d548f13f89
Doug Jones <dsjr1284@gmail.com>
2020-08-10 16:11:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
-
+require "pry"
 html = open("https://flatironschool.com/")
 
 Nokogiri :: HTML(html)
 
 doc = Nokogiri :: HTML(html)
+binding.pry
 
-puts doc
+
